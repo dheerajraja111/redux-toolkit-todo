caefe295b6ba462bdf6ade6b108785dd91cd52d3
Dheeraj Raja <dheraja@WKMZT9E5E46D.global.publicisgroupe.net>
2023-06-03 00:45:45 +0530

    redux toolkit example added

---
+++ src/components/Todos.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import Todo from "./Todo";
+
+export default function Todos() {
+  const todos = useSelector((state) => state.todos);
+  console.log(todos);
+
+  return (
+    <div>
+      <ul>
+        {todos.map((todo) => {
+          <li key={todo.id}>
+            <Todo todo={todo} />
+          </li>;
+        })}
+      </ul>
+    </div>
+  );
+}
+
